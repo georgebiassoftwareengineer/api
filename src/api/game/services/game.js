b8f007f4bddd2fe876fb8c6539e76500ede37833
georgebiassoftwareengineer <georgebiassoftwareengineer@gmail.com>
2022-01-12 19:51:09 -0300

    Create publisher/developer via service

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
@@ -13,18 +14,34 @@ async function getGameInfo(slug) {
   return {
     rating: "BR0",
     short_description: description.textContent.slice(0, 160),
-    description: description.innerHTML
+    description: description.innerHTML,
   };
 }
 
-module.exports = () => ({
-  async populate() {
+module.exports = ({ strapi }) => ({
+  async populate(params) {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
 
     const {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(await getGameInfo(products[0].slug));
+    console.log(products[0]);
+
+    await strapi.entityService.create("api::publisher.publisher", {
+      data: {
+        name: products[0].publisher,
+        slug: slugify(products[0].publisher).toLowerCase(),
+      },
+    });
+
+    await strapi.entityService.create("api::developer.developer", {
+      data: {
+        name: products[0].developer,
+        slug: slugify(products[0].developer).toLowerCase(),
+      },
+    });
+
+    // console.log(await getGameInfo(products[0].slug));
   },
 });
