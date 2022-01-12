29214bf756817011352c529dc333174933e90e4f
georgebiassoftwareengineer <georgebiassoftwareengineer@gmail.com>
2022-01-12 02:54:17 -0300

    feat: change name, description and icon to plugin

--- src/plugins/repositories/admin/src/components/PluginIcon/index.js
+++ src/plugins/repositories/admin/src/components/PluginIcon/index.js
@@ -5,8 +5,8 @@
  */
 
 import React from 'react';
-import Puzzle from '@strapi/icons/Puzzle';
+import Code from '@strapi/icons/Code';
 
-const PluginIcon = () => <Puzzle />;
+const PluginIcon = () => <Code />;
 
 export default PluginIcon;
