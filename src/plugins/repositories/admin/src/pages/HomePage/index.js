ae092101b3609a7bcb4160df5a6701a9fe60eafa
georgebiassoftwareengineer <georgebiassoftwareengineer@gmail.com>
2022-01-12 04:26:46 -0300

    feat: list repositories plugin

--- src/plugins/repositories/admin/src/pages/HomePage/index.js
+++ src/plugins/repositories/admin/src/pages/HomePage/index.js
@@ -1,19 +1,91 @@
-/*
- *
- * HomePage
- *
- */
+import React, { useState, useEffect, memo } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  td,
+  th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const Paragraph = styled.p`
+  margin-bottom: 30px;
+`;
 
 const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/React-Avancado/repos")
+      .then((res) => setRows(res.data))
+      .catch((e) => console.error("Ops... github API limit exceeded.", e));
+  }, []);
+
+  const headers = [
+    {
+      name: "Name",
+      value: "name",
+    },
+    {
+      name: "Description",
+      value: "description",
+    },
+    {
+      name: "Url",
+      value: "html_url",
+    },
+  ];
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Title>React Avançado Repositories</Title>
+      <Paragraph>
+        A list of our repositories in React Avançado course.
+      </Paragraph>
+
+      <table>
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header.name}</th>
+            ))}
+          </tr>
+        </thead>
+        {rows.map((data, index) => (
+          <tbody key={index}>
+            <tr>
+              <>
+                <td >{data.name}</td>
+                <td>{data.description}</td>
+                <td>{data.html_url}</td>
+              </>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </Wrapper>
   );
 };
 
